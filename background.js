485d0147867454eb45da9c7be63604c5f64d66d3
Dan Pham <danphamx@gmail.com>
2026-01-30 11:48:47 +0800

    Change menu to sidebar, I think this is a Google best practice?

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
+// Open the side panel when the extension icon is clicked
+chrome.action.onClicked.addListener((tab) => {
+  chrome.sidePanel.open({ tabId: tab.id });
+});
+
 // Listen for messages from content.js (the floating cupcake)
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'OPEN_PANEL') {
     // Open the side panel in the current window
-    chrome.sidePanel.open({ windowId: sender.tab.windowId });
+    chrome.sidePanel.open({ tabId: sender.tab.id });
   }
 });
